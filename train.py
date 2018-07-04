add EOS to masks and fix masking bug

train.py
@@ -2,7 +2,7 @@
 # @Author: aaronlai
 # @Date:   2018-05-14 19:08:20
 # @Last Modified by:   AaronLai
-# @Last Modified time: 2018-07-02 21:41:00
+# @Last Modified time: 2018-07-03 22:33:55
 
 
 from utils import init_embeddings, compute_loss, compute_perplexity, \
@@ -122,6 +122,8 @@ def main(args):
     target_data = loadfile(t_filename, is_source=False,
                            max_length=t_max_leng) + embed_shift
     masks = (target_data >= embed_shift)
+    masks = np.append(np.ones([len(masks), 1], dtype=bool), masks, axis=1)
+    masks = masks[:, :-1]
     n_data = len(source_data)
 
     dev_source_data = None
@@ -131,6 +133,9 @@ def main(args):
         dev_target_data = loadfile(dev_t_filename, is_source=False,
                                    max_length=t_max_leng) + embed_shift
         dev_masks = (dev_target_data >= embed_shift)
+        dev_masks = np.append(
+            np.ones([len(dev_masks), 1], dtype=bool), dev_masks, axis=1)
+        dev_masks = dev_masks[:, :-1]
     print("\tDone.")
 
     # Training

train_ECM.py
@@ -2,7 +2,7 @@
 # @Author: aaronlai
 # @Date:   2018-05-14 19:08:20
 # @Last Modified by:   AaronLai
-# @Last Modified time: 2018-07-02 21:43:00
+# @Last Modified time: 2018-07-03 22:35:13
 
 
 from utils import init_embeddings, compute_ECM_loss, compute_perplexity, \
@@ -128,6 +128,8 @@ def main(args):
         c_filename, header=None, index_col=None, dtype=int)[0].values
 
     masks = (target_data >= embed_shift)
+    masks = np.append(np.ones([len(masks), 1], dtype=bool), masks, axis=1)
+    masks = masks[:, :-1]
     n_data = len(source_data)
 
     dev_source_data = None
@@ -146,6 +148,9 @@ def main(args):
             dev_c_filename, header=None, index_col=None, dtype=int)[0].values
 
         dev_masks = (dev_target_data >= embed_shift)
+        dev_masks = np.append(
+            np.ones([len(dev_masks), 1], dtype=bool), dev_masks, axis=1)
+        dev_masks = dev_masks[:, :-1]
     print("\tDone.")
 
     # Training

utils.py
@@ -2,7 +2,7 @@
 # @Author: aaronlai
 # @Date:   2018-05-14 23:54:40
 # @Last Modified by:   AaronLai
-# @Last Modified time: 2018-05-25 18:11:13
+# @Last Modified time: 2018-07-03 22:43:33
 
 
 from model.encoder import build_encoder
@@ -72,7 +72,7 @@ def compute_loss(source_ids, target_ids, sequence_mask, embeddings,
             losses = tf.nn.sparse_softmax_cross_entropy_with_logits(
                 logits=train_logits, labels=final_ids)
 
-            losses = tf.boolean_mask(losses, sequence_mask)
+            losses = tf.boolean_mask(losses[:, :-1], sequence_mask)
             reduced_loss = tf.reduce_mean(losses)
             CE = tf.reduce_sum(losses)
 
@@ -144,13 +144,13 @@ def compute_ECM_loss(source_ids, target_ids, sequence_mask, choice_qs,
             alpha_reg = tf.reduce_mean(choice_qs * -tf.log(alphas))
             int_mem_reg = tf.reduce_mean(tf.norm(int_M_emo, axis=1))
 
-            losses = tf.boolean_mask(losses, sequence_mask)
+            losses = tf.boolean_mask(losses[:, :-1], sequence_mask)
             reduced_loss = tf.reduce_mean(losses) + alpha_reg + int_mem_reg
 
             # prepare for perplexity computations
             CE = tf.nn.sparse_softmax_cross_entropy_with_logits(
                 logits=train_log_probs, labels=final_ids)
-            CE = tf.boolean_mask(CE, sequence_mask)
+            CE = tf.boolean_mask(CE[:, :-1], sequence_mask)
             CE = tf.reduce_sum(CE)
 
             train_outs = (cell, train_log_probs, alphas, int_M_emo)